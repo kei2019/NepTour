9639db930250aca16f1b6db3f8bc9f193c30a56b
kshitij kafle <kshitijkafle10@gmail.com>
2019-08-24 16:39:12 +0545

    ninth commit

--- nepTour.py
+++ nepTour.py
@@ -4,23 +4,23 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def get_title_from_index(index):
-    return df[df.index==index]["VDC_name"].values[0]
-def get_index_from_title(VDC_name):
-    return df[df.VDC_name==VDC_name]["index"].values[0]
+    return df[df.index==index]["place_name"].values[0]
+def get_index_from_title(place_name):
+    return df[df.place_name==place_name]["index"].values[0]
 
-df=pd.read_csv("vdc.csv")
+df=pd.read_csv("place.csv")
 
 #print(df.head())
-features=['Region', 'Geographical Region', 'District', 'Zone']
+features=['Zone', 'District', 'place_type']
 
-#for feature in features:
- #   df[feature]=df[feature].fillna(' ')
+for feature in features:
+    df[feature]=df[feature].fillna(' ')
 
 def combine_features(row):
-    return row['Region']+" "+row['Geographical Region']+" "+row['District']+" "+row['Zone']
+    return row['Zone']+" "+row['District']+" "+row['place_type']
 
 df["combine_features"]=df.apply(combine_features, axis=1)
-print (df["combine_features"].head())
+#print (df["combine_features"].head())
 
 cv=CountVectorizer()
 count_matrix=cv.fit_transform(df["combine_features"])
@@ -29,7 +29,7 @@ count_matrix=cv.fit_transform(df["combine_features"])
 #
 
 cos_sim=cosine_similarity(count_matrix)
-liked_place="Ankhop"
+liked_place="itahari"
 
 place_index=get_index_from_title(liked_place)
 similar_places=list(enumerate(cos_sim[place_index]))
@@ -41,7 +41,7 @@ i=0
 for place in sorted_similar_places:
     print(get_title_from_index(place[0]))
     i=i+1
-    if i>10:
+    if i>20:
         break
 
 
