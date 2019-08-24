cc5e2ccd290edcb1b619155f0bb8a3460dec02dc
kshitij kafle <kshitijkafle10@gmail.com>
2019-08-24 13:15:50 +0545

    eighth commit

--- nepTour.py
+++ nepTour.py
@@ -3,10 +3,10 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def get_title_from_index(VDC_code):
-    return df[df.VDC_code==VDC_code]["VDC_name"].values[0]
+def get_title_from_index(index):
+    return df[df.index==index]["VDC_name"].values[0]
 def get_index_from_title(VDC_name):
-    return df[df.VDC_name==VDC_name]["VDC_code"].values[0]
+    return df[df.VDC_name==VDC_name]["index"].values[0]
 
 df=pd.read_csv("vdc.csv")
 
