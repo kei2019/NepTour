54e8b429df65e443da088abb7ea5dc9660e326c1
kshitij kafle <kshitijkafle10@gmail.com>
2019-08-24 12:22:21 +0545

    third commit

--- nepTour.py
+++ nepTour.py
@@ -8,3 +8,14 @@ def get_title_from_index(District_code):
 def get_index_from_title(VDC_name):
     return df[df.VDC_name==VDC_name]["District_code"].values[0]
 
+df=pd.read_csv("vdc.csv")
+
+#print(df.head())
+features=['Region', 'Geographical Region']
+
+for feature in features:
+    df[feature]=df[feature].fillna(' ')
+
+def combine_features(row):
+    return row['Region']+" "+row['Geographical Region']
+
