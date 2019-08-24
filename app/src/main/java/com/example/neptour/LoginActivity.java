b3f3efe55090f448213a1551a099b4efe5eb3bd9
mandeep <keibanau@gmail.com>
2019-08-24 15:14:25 +0545

    initial commit

---
+++ app/src/main/java/com/example/neptour/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.example.neptour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button login_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        login_btn = findViewById(R.id.login_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFieldEmpty(email) && isFieldEmpty(password)) {
+                    if (isEmailValid(email)) {
+                        String emailValue = email.getText().toString().trim();
+                        String passValue = password.getText().toString().trim();
+                        Toast.makeText(LoginActivity.this, "email " +emailValue, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+    }
+    public Boolean isFieldEmpty(EditText view) {
+        if (view.getText().toString().length() > 0) {
+            return true;
+        } else {
+            view.setError("Field Required");
+            return false;
+        }
+    }
+
+    public Boolean isEmailValid(EditText view) {
+        String value = view.getText().toString();
+        if (Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+            return true;
+        } else {
+            view.setError("Invalid email");
+            return false;
+        }
+
+    }
+}
